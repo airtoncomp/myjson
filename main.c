0d4134b8d4ab8208ec37c31101c857df1e0781a4
airtoncomp <airtonishimori@gmail.com>
2026-06-13 00:24:23 -0300

    Call test()

---
+++ main.c
@@ -0,0 +1,17 @@
+/**
+ * 2026, A personal json parser named MyJSON
+ * Author: Airton Ishimori
+ */
+
+#include <stdio.h>
+
+#include "myjson.h"
+
+extern void test();
+
+int main () 
+{
+    test();
+    printf("myjson lib\n");
+    return 0;
+}
