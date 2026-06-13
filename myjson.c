611fe37b133b1f4c27325d44602c40ca472f5cc4
airtoncomp <airtonishimori@gmail.com>
2026-06-13 00:21:50 -0300

    Add macro utilities and scan a couple of tokens (TODO)

--- myjson.c
+++ myjson.c
@@ -19,19 +19,34 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 #include <assert.h>
+#include <ctype.h>
 
-#define MJ_RET_ON_NULL(x, fmt, ...) do {                \
-            if (x == NULL) {                            \
-                fprintf(stderr, fmt, ##__VA_ARGS__);    \
-                return -1;                              \
-            }                                           \
+#define MJ_RET_ON_NULL(x, fmt, ...) do {                    \
+            if (x == NULL) {                                \
+                fprintf(stderr, fmt"\n", ##__VA_ARGS__);    \
+                return -1;                                  \
+            }                                               \
         } while (0)
 
+#define MJ_RET_ON_ERR(x, fmt, ...) do {                     \
+            if (x < 0) {                                    \
+                fprintf(stderr, fmt"\n", ##__VA_ARGS__);    \
+                return -1;                                  \
+            }                                               \
+        } while (0)
+
+#define MJ_RET_ON_ERR2(x) do {                              \
+            if (x < 0) {                                    \
+                return -1;                                  \
+            }                                               \
+        } while (0)
 
 typedef enum {
     MJTOK_IDENTIFIER,               /* aA-zZ */
@@ -64,9 +79,16 @@ typedef struct {
 #define MJTOK_ARRAY_INIT_CAP        100
 #define MJTOK_ARRAY_GROWTH_FACTOR   2
 
+#define is_brace_open(x)            (x == '{')
+#define is_brace_close(x)           (x == '}')
+#define is_bracket_open(x)          (x == '[')
+#define is_bracket_close(x)         (x == ']')
+#define is_colon(x)                 (x == ':')
+
 static void init_tok_arr(mjarr_t *arr) 
 {
     assert(arr != NULL && "Array cannot be null");
+
     arr->count = 0;
     arr->cap = MJTOK_ARRAY_INIT_CAP;
     arr->tokens = malloc(arr->cap * sizeof(*arr->tokens));
@@ -74,12 +96,68 @@ static void init_tok_arr(mjarr_t *arr)
 
 static int append_tok(mjarr_t *arr, mjtok_t token)
 {
+    assert(arr != NULL && "Array cannot be null");
+
     if (arr->count >= arr->cap) {
         size_t new_cap = arr->cap * MJTOK_ARRAY_GROWTH_FACTOR;
-        MJ_RET_ON_NULL(realloc(arr->tokens, new_cap * sizeof(*arr->tokens)), "Out of memory failure");
+        mjtok_t *tmp = realloc(arr->tokens, new_cap * sizeof(*arr->tokens));
+        MJ_RET_ON_NULL(tmp, "Out of memory failure");
+        arr->tokens = tmp;
+        arr->cap = new_cap;
     }
 
     arr->tokens[arr->count++] = token;
+
+    return 0;
+}
+
+static inline mjtok_t new_tok(mjtok_type_t t, const void *val, const char *pos, size_t val_len)
+{
+    mjtok_t token = {
+            .type = t,
+            .value = val,
+            .start = pos, 
+            .len = val_len
+        };
+
+    return token;
+}
+
+static int tokenize_json (mjarr_t *arr, char *const json)
+{
+    char *cptr = json; 
+
+    while (cptr && *cptr != '\0') {
+        printf("char: %c\n", *cptr);
+        if (isspace((unsigned char) *cptr) || *cptr == '\t') {
+            goto advance;
+        }
+        if (is_brace_open(*cptr)) {
+            MJ_RET_ON_ERR2(append_tok(arr, new_tok(MJTOK_SEP_BRACE_OPEN, cptr, cptr, 1)));
+            goto advance;
+        }
+        if (is_brace_close(*cptr)) {
+            MJ_RET_ON_ERR2(append_tok(arr, new_tok(MJTOK_SEP_BRACE_CLOSE, cptr, cptr, 1)));
+            goto advance;
+        }
+        if (is_colon(*cptr)) {
+            MJ_RET_ON_ERR2(append_tok(arr, new_tok(MJTOK_SEP_COLON, cptr, cptr, 1)));
+            goto advance;
+        }
+        if (is_bracket_open(*cptr)) {
+            MJ_RET_ON_ERR2(append_tok(arr, new_tok(MJTOK_SEP_BRACE_OPEN, cptr, cptr, 1)));
+            goto advance;
+        }
+        if (is_bracket_close(*cptr)) {
+            MJ_RET_ON_ERR2(append_tok(arr, new_tok(MJTOK_SEP_BRACE_CLOSE, cptr, cptr, 1)));
+            goto advance;
+        }
+        //TODO
+advance:
+        cptr++;
+    }
+
+    return 0;
 }
 
 void test() {
@@ -92,4 +170,22 @@ void test() {
         .len = 1
     };
     append_tok(&array, token);
+
+    mjtok_t token2 = {
+        .type = MJTOK_IDENTIFIER,
+        .value = "b",
+        .start = 0,
+        .len = 1
+    };
+    append_tok(&array, token2);
+
+    mjarr_t arr2; 
+    init_tok_arr(&arr2);
+    char buf[] = "{\"a\":\"b\", \"c\":[]}";
+    char *str = buf;
+    tokenize_json(&arr2, str);
+
+    for (int i = 0; i < arr2.count; i++)
+        printf("token: type=%d, value=%c, start=%p, len=%zu\n",
+                arr2.tokens[i].type, *(char *) arr2.tokens[i].value, arr2.tokens[i].start, arr2.tokens[i].len);
 }
