52aa0e6d71549d8508f4b29954c5b6bad68cb148
airtoncomp <airtonishimori@gmail.com>
2026-08-02 20:58:16 -0300

    Add MJ_STR(x) macro helper to stringizing c string into json string. The macro literally consider " in string. Ex: MJ_STR("a") outputs \"a\", this is the same as writing \"a\" manually. Then, when printf() prints escaped string, it will print as "a", not a.

--- myjson.h
+++ myjson.h
@@ -24,6 +24,8 @@
 
 #include <string.h>
 
+#define MJ_STR(x) #x
+
 /**
  * Public JSON value type exposed by the library.
  * Users should refer to this type through the myjson_t
@@ -35,9 +37,23 @@ typedef struct myjson myjson_t;
  * Public.
  * User should use 'myjson_t' as main variable to represent 
  * json data and make call to functions that use this type.
+ * The 'create' functions make memory allocation while
+ * 'free' ones destroy allocated memories.
  */
 
 myjson_t *myjson_create();
+myjson_t *myjson_create_root();
+myjson_t *myjson_create_obj(size_t cap);
+myjson_t *myjson_create_pair_str(const char *key, char *val);
+//myjson_t *myjson_create_pair_int(const char *key, int val); //TODO
+//myjson_t *myjson_create_pair_double(const char *key, double val); //TODO
+//myjson_t *myjson_create_pair_true(const char *key); //TODO
+//myjson_t *myjson_create_pair_false(const char *key); //TODO
+myjson_t *myjson_create_arr(size_t cap);
+
+void myjson_add_obj_to_root(myjson_t **root, myjson_t *obj);
+void myjson_add_pair_to_obj(myjson_t *obj, myjson_t *pair);
+
 int myjson_parse(myjson_t *mj, const char *json);
 void myjson_print(const myjson_t *mj);
 void myjson_free(myjson_t *mj);
