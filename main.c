d9ced7719285bb7d352fe22ede34ee0b4635eb70
airtoncomp <airtonishimori@gmail.com>
2026-08-21 20:20:47 -0300

    Add test to create json int and double.

--- main.c
+++ main.c
@@ -8,7 +8,8 @@
 
 #include "myjson.h"
 
-void test() {
+void test() 
+{
     //char *str = R"({"a":"b", "c":[], "d":1, "e":2.4, "f":0.0e10})";
     //char *str = "{\"a\":\"b\", \"c\":[]}";
     //char *str = R"({"a":0.0e12, "b":0.1E-56, "c":2.5e+4, "d":6e7})";
@@ -19,6 +20,7 @@ void test() {
     //char *str = R"({"a":[true]})";
     //char *str = R"({"a":[{"b":"c"},{"d":true}, "elem"]})";
     //char *str = R"(["a",true])";
+    //char *str = R"({"a":"c"})";
     char *str = R"({"a":2})";
 
     printf("%s\n", str);
@@ -26,12 +28,43 @@ void test() {
     myjson_t *mj = myjson_create();
     myjson_parse(mj, str);
     myjson_print(mj);
-    myjson_free(mj);
+    myjson_free_root(mj);
+}
+
+void test_object()
+{
+    printf("\n--- BEGIN: TEST OBJECT CREATION ---\n");
+
+    myjson_t *mj = myjson_create_root();
+    myjson_t *obj = myjson_create_obj(3);
+    myjson_t *str_pair = myjson_create_pair_str(MJ_STR("a"), MJ_STR("\r"));
+    myjson_t *int_pair = myjson_create_pair_int(MJ_STR("b"), 45);
+    myjson_t *double_pair = myjson_create_pair_double(MJ_STR("c"), 32.5e-4);
+
+    myjson_add_pair_to_obj(obj, str_pair);
+    myjson_add_pair_to_obj(obj, int_pair);
+    myjson_add_pair_to_obj(obj, double_pair);
+    myjson_add_obj_to_root(&mj, obj);
+    
+    myjson_print(mj);
+    myjson_free(obj);
+    myjson_free(str_pair);
+    myjson_free(int_pair);
+    myjson_free(double_pair);
+    myjson_free_root(mj);
+
+    printf("\n--- END: TEST OBJECT CREATION ---\n");
 }
 
 int main () 
 {
     test();
-    printf("\nmyjson lib\n");
+    printf("\n");
+
+    test_object();
+    printf("\n");
+
+    printf("myjson lib\n");
+
     return 0;
 }
