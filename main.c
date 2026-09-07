f42552c840f2a5700eefb40110dd782bf385cac6
airtoncomp <airtonishimori@gmail.com>
2026-09-07 16:01:37 -0300

    Add test case for replacing elements in json array

--- main.c
+++ main.c
@@ -56,7 +56,6 @@ void test_object()
     myjson_add_pair_to_obj(obj, null_pair);
 
     myjson_del_pair_from_obj(obj, MJ_STR("c"));
-    myjson_update_str_pair_in_obj(obj, MJ_STR("f"), "true"); // should fail
     myjson_update_str_pair_in_obj(obj, MJ_STR("a"), MJ_STR("test"));
     myjson_update_int_pair_in_obj(obj, MJ_STR("b"), 90);
     myjson_update_double_pair_in_obj(obj, MJ_STR("c"), -12.9);
@@ -121,6 +120,13 @@ void test_array()
     myjson_t *obj = myjson_create_obj(1);
     myjson_t *str_pair = myjson_create_pair_str(MJ_STR("a"), MJ_STR("\r"));
     myjson_add_pair_to_obj(obj, str_pair);
+
+    myjson_replace_arr_elem_w_int(arr, 0, 678);
+    myjson_replace_arr_elem_w_double(arr, 1, 33.2e+2);
+    myjson_replace_arr_elem_w_true(arr, 1);
+    myjson_replace_arr_elem_w_false(arr, 1);
+    myjson_replace_arr_elem_w_null(arr, 1);
+    myjson_replace_arr_elem_w_str(arr, 0, MJ_STR("testing"));
 
     myjson_append_obj_to_arr(arr, obj);
 
