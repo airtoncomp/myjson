7ed94720c2baf9bdd55a0d543cd566c07ca23e69
airtoncomp <airtonishimori@gmail.com>
2026-09-13 02:31:10 -0300

    Add myjson_get_obj_pair() to retrieve pair ("key":val) from a json object.

--- myjson.h
+++ myjson.h
@@ -81,7 +81,7 @@ void myjson_update_int_pair_in_obj(myjson_t *obj, const char *key, int val);
 void myjson_update_double_pair_in_obj(myjson_t *obj, const char *key, double val);
 void myjson_update_bool_pair_in_obj(myjson_t *obj, const char *key, int zero_or_one);
 
-const myjson_t *myjson_get_item(const myjson_t *root, const char *key);
+const myjson_t *myjson_get_obj_pair(const myjson_t *root, const char *key);
 
 int myjson_parse(myjson_t *mj, const char *json);
 void myjson_print(const myjson_t *mj);
